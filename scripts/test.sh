16e5b2c23fc91cf43e8e7f61745f08e6a8e5fb70
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2025-01-02 22:02:42 +0100

    build: log test output

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,27 @@ set -e
 set -o pipefail
 cd "${DN}/.."
 
-mkdir -p tmp
-go test ./... "$@" -cover -coverprofile=tmp/coverage.out
+LOG=$(mktemp)
+
+exitHandler() {
+  trap - "EXIT"
+  rm -f "${LOG}"
+}
+
+trap exitHandler "EXIT"
+
+if ! go test ./... -cover -coverprofile=tmp/coverage.out -v &>"${LOG}"; then
+  awk '
+    function color(c,s) {
+            printf("\033[%dm%s\033[0m\n",30+c,s)
+    }
+    /(^ok|coverage:)\s/ {next}
+    /FAIL:/ {color(1,$0);next}
+    /PASS:/ {color(2,$0);next}
+    {print}
+  ' "${LOG}"
+  echo "test: FAILED"
+  exit 1
+fi
 
 echo "test: SUCCEEDED"
